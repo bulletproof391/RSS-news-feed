4d776d98e2bd2367e2b543e045c4fbed4ffdf916
Dmitry <bulletproof024@gmail.com>
2018-06-30 14:29:54 +0700

    Interface building

---
+++ RSS news feed/RSSFeedTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  RSSFeedTableViewCell.swift
+//  RSS news feed
+//
+//  Created by Дмитрий Вашлаев on 30.06.18.
+//  Copyright © 2018 Дмитрий Вашлаев. All rights reserved.
+//
+
+import UIKit
+
+class RSSFeedTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var newsImage: UIImageView!
+    @IBOutlet weak var newsTitle: UILabel!
+    @IBOutlet weak var newsSource: UILabel!
+    @IBOutlet weak var newsDescription: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
